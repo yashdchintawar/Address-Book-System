f5c56c445fbefad8ab267f1bcbe8c3ea294ed941
yashdchintawar <yashchintawar31@gmail.com>
2022-03-15 22:05:15 +0530

    added class a=now we can create address contact

--- src/com/addressbook/AddressBookSystem.java
+++ src/com/addressbook/AddressBookSystem.java
@@ -3,6 +3,17 @@ package com.addressbook;
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book System Program");
+        AddressBookData contact1 = new AddressBookData();
 
     }
+}
+class AddressBookData{
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zipCode;
+    String PhoneNumber;
+    String eMail;
 }
