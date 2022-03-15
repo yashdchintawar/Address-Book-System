81d2ba19c1a3092225891006c41006ee3046e167
yashdchintawar <yashchintawar31@gmail.com>
2022-03-15 22:03:43 +0530

    welcome start message

---
+++ src/com/addressbook/AddressBookSystem.java
@@ -0,0 +1,8 @@
+package com.addressbook;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book System Program");
+
+    }
+}
